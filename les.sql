7cfb2e13b629bb0d8dfd1f6b5f15a95e2f4d4f35
Kot-Olen <125899067+Kot-Olen@users.noreply.github.com>
2023-02-20 20:12:16 +0200

    first commit

---
+++ les.sql
@@ -0,0 +1,15 @@
+insert into lesson_22.user_23 (username)
+values ('Olen Kot');
+
+select * from lesson_22.user_23;
+
+insert into lesson_22.user_23 (email)
+values ('OlenKot@gmail.com');
+
+insert into lesson_22.user_23 (id, username, email, city)
+values (3, 'OlenOlen', 'OlenKot@gmail.com', 'Odesa');
+
+insert into lesson_22.user_23 (id, username, email, city)
+values (4, 'OlenOlen', 'OlenKot@gmail.com', 'Odesa'),
+(5, 'OlennOlen', 'OlenKot5@gmail.com', 'Odesa'),
+(6, 'OlennOlen', 'OlenKot6@gmail.com', 'Odesa');
